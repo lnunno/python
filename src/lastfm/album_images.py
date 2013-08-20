e3202421298e0574ff19f7c60a3553fbff18865c
lnunno <lucasnunno@gmail.com>
2013-08-19 23:57:56 -0600

    Refactoring

--- src/lastfm/album_images.py
+++ src/lastfm/album_images.py
@@ -7,17 +7,15 @@ import urllib as ul
 from bs4 import BeautifulSoup
 import os
 
-def get_image_tags(user,api_key,image_size="extralarge"):
+def top_album_image_urls(user,api_key,image_size="extralarge"):
     url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=%s&api_key=%s" % (user,api_key)
     html = ul.urlopen(url).read()
     soup = BeautifulSoup(html)
     image_links = [link.text for link in soup.find_all('image',{'size':image_size})]
-    i = 1
-    for link in image_links:
-        filename = str(i).zfill(4) + '.' + os.path.splitext(link)[1]
-        ul.urlretrieve(link, os.path.join('output',filename))
-        i += 1
+    return image_links
         
-    
-if __name__ == '__main__':
-    get_image_tags('USERNAME','API_KEY')
+def save_url(url,outpath):
+    if os.path.split(outpath)[1] == '':
+        outpath = os.path.join(outpath,os.path.split(url)[1])
+    ul.urlretrieve(url, outpath)
+    print 'Saved url to %s' % (os.path.abspath(outpath))
