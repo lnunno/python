ed572e751cc47dae6c257bf12579077b32566674
lnunno <lucasnunno@gmail.com>
2013-09-20 09:11:21 -0600

    Added unified grading file support.

--- src/grading/grading_utils.py
+++ src/grading/grading_utils.py
@@ -4,6 +4,7 @@ Created on Sep 17, 2013
 @author: lnunno
 '''
 import os
+import re
 import subprocess
 from datetime import datetime
 
@@ -25,12 +26,26 @@ def mirror_directory_tree(input_root,output_root):
 def tar_extract_specific(tar_file,elements):
     for e in elements:
         print command('tar -xzvf %s %s' % (tar_file,e))
+        
+def compile_java(java_src_file,classpaths=None):
+    classpaths = classpaths or []
+    classpaths.append('.')
+    path = ':'.join(classpaths)
+    command('javac -cp %s %s' % (path,java_src_file))
                 
-def run_junit3(test_file):
-    command('java -cp %s junit.textui.TestRunner %s' % (junit3,test_file))
+def run_junit3(test_file,classpaths=None):
+    classpaths = classpaths or []
+    classpaths.append('.')
+    classpaths.append(junit3)
+    path = ':'.join(classpaths)
+    command('java -cp %s junit.textui.TestRunner %s' % (path,test_file))
     
-def run_junit4(test_file):
-    command('java -cp %s org.junit.runner.JUnitCore %s' % (junit4,test_file))
+def run_junit4(test_file,classpaths=None):
+    classpaths = classpaths or []
+    classpaths.append('.')
+    classpaths.append(junit4)
+    path = ':'.join(classpaths)
+    command('java -cp %s org.junit.runner.JUnitCore %s' % (path,test_file))
                 
 def command(cmd):
     try:
@@ -54,6 +69,43 @@ def get_students(student_file):
         students = f.readlines()
         students = map(lambda x: x.strip(), students)
     return students
+
+def make_unified_grading_file(student_list,dst,additional_content=''):
+    with open(dst,'w') as f:
+        for s in student_list:
+            content = '''\
+---
+USERNAME: %s
+%s
+TOTAL: GRADE/100
+''' % (s,additional_content) 
+            f.write(content)
+    print "Wrote grading file to",os.path.abspath(dst)
+            
+
+def _make_grades_file(outroot, acc, oldusername):
+    p = os.path.join(outroot, oldusername)
+    if not os.path.exists(p):
+        os.makedirs(p)
+    with open(os.path.join(p, 'grade.txt')) as f:
+        f.write(acc)
+
+def split_grading_file(grading_file,outroot):
+    gf = open(grading_file)
+    acc = ''
+    oldusername = ''
+    for line in gf.readlines():
+        m = re.match('USERNAME: (\w+)', line)
+        if m:
+            username = m.group(1).strip()
+            if acc and oldusername:
+                _make_grades_file(outroot, acc, oldusername)
+                oldusername = username
+                acc = ''
+        else:
+            acc += line
+            continue
+    _make_grades_file(outroot, acc, username)
         
 def main():
     pass
